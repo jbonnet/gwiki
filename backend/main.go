4b8030d8d2fa905cb526eaa4c27c557924038fcc
José Bonnet <jbonnet@alticelabs.com>
2021-04-03 17:34:25 +0100

    Add GET a specific snippet and create a snippet

--- backend/main.go
+++ backend/main.go
@@ -8,6 +8,15 @@ import (
 // Define a home handler function which writes a byte slice containing
 // "Hello from GWiki" as the response body.
 func home(w http.ResponseWriter, r *http.Request) {
+  // Check if the current request URL path exactly matches "/". If it doesn't, use
+  // the http.NotFound() function to send a 404 response to the client.
+  // Importantly, we then return from the handler. If we don't return the handler
+  // would keep executing and also write the "Hello from SnippetBox" message.
+  if r.URL.Path != "/" {
+    http.NotFound(w, r)
+    return
+  }
+
   w.Write([]byte("Hello from GWiki"))
 }
 
@@ -26,6 +35,11 @@ func main() {
   // register the home function as the handler for the "/" URL pattern.
   mux := http.NewServeMux()
   mux.HandleFunc("/", home)
+
+  // Register the two new handler functions and corresponding URL patterns with // the servemux, in exactly the same way that we did before.
+  mux.HandleFunc("/snippet", showPageSnippet)
+  mux.HandleFunc("/snippet/create", createPageSnippet)
+
 
   // Use the http.ListenAndServe() function to start a new web server. We pass in
   // two parameters: the TCP network address to listen on (in this case ":4000")
